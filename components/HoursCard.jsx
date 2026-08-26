08aeaef1f7164132a71a8e7c74bf5fa52f7d31aa
bradbates33-sketch <bradbates33@gmail.com>
2026-08-26 11:27:11 +1000

    Update HoursCard.jsx

--- components/HoursCard.jsx
+++ components/HoursCard.jsx
@@ -28,7 +28,7 @@ export default function HoursCard() {
         <span>(02) 4341 0000</span>
       </div>
 
-      <a
+      
         href="tel:0243410000"
         className="mt-[18px] block text-center w-full bg-ink text-paper font-mono text-xs tracking-wide uppercase py-3 rounded-[2px] hover:bg-chili transition-colors"
       >
@@ -36,6 +36,4 @@ export default function HoursCard() {
       </a>
     </div>
   );
-}
-
 }
